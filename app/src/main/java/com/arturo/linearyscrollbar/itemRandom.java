ae32e14f443f089cdc94581ebfec095ba559d14c
Tofiter <christopheradan9@gmail.com>
2018-08-05 01:13:31 -0500

    Pull pal plebe

--- app/src/main/java/com/arturo/linearyscrollbar/itemRandom.java
+++ app/src/main/java/com/arturo/linearyscrollbar/itemRandom.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class itemRandom extends AppCompatActivity {
     private Button regreso;
+    private TextView godNameTitle;
+    private ImageView godImage;
+    private LinearLayout ln1;
+    private LinearLayout ln2;
+    private LinearLayout ln3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +25,17 @@ public class itemRandom extends AppCompatActivity {
     }
 
     public void initComponents(){
+        godNameTitle = new TextView(this);
+        godImage = new ImageView(this);
         regreso = new Button(this);
-
+        ln1 = new LinearLayout(this);
+        ln2 = (LinearLayout) findViewById(R.id.linearabajo2);
+        ln3 = new LinearLayout(this);
+        ln1.setOrientation(LinearLayout.HORIZONTAL);
+        ln1.setBackgroundColor(getResources().getColor(R.color.Negro));
+        ln2.setBackgroundColor(getResources().getColor(R.color.Negro));
+        ln3.setOrientation(LinearLayout.HORIZONTAL);
+        ln3.setBackgroundColor(getResources().getColor(R.color.Negro));
         regreso.setText(R.string.regresar);
         regreso.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL);
         regreso.setTextSize(25);
