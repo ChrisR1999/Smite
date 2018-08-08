c806d6384826f2a0fbf4c4a4743cb03f759dcf2f
Arturo <r20a@hotmail.com>
2018-08-07 19:18:32 -0500

    asd
    asd

---
+++ app/src/main/java/com/arturo/linearyscrollbar/DotosDeLosItems.java
@@ -0,0 +1,13 @@
+package com.arturo.linearyscrollbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DotosDeLosItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dotos_de_los_items);
+    }
+}
